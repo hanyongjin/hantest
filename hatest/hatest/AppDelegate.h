3e11caeb0338d5231eea9265e28e181b84f27741
韩永进 <1018971016@qq.com>
2019-04-18 16:19:36 +0800

    初始化

---
+++ hatest/hatest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hatest
+//
+//  Created by mac on 2019/4/18.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hatest/hatest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  hatest
+//
+//  Created by mac on 2019/4/18.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
